0e582e9ff9b0f14ad68cec5a6ba98fa2a4f3d963
Reinout van Rees <reinout@vanrees.org>
2013-08-15 16:32:49 +0200

    Added bare-bones start of netcdf file reading including demo page

--- lizard_ocean/views.py
+++ lizard_ocean/views.py
@@ -6,9 +6,9 @@ from __future__ import print_function
 from django.utils.translation import ugettext as _
 # from django.core.urlresolvers import reverse
 from lizard_map.views import MapView
-# from lizard_ui.views import UiView
+from lizard_ui.views import UiView
 
-# from lizard_ocean import models
+from lizard_ocean import netcdf
 
 
 # class TodoView(UiView):
@@ -21,3 +21,13 @@ class MainView(MapView):
     """Main view of the application."""
     template_name = 'lizard_ocean/main.html'
     # page_title = _('')
+
+
+class RawNetcdfView(UiView):
+    """Raw debug view of the netcdf data."""
+    template_name = 'lizard_ocean/raw_netcdf.html'
+    # page_title = _('')
+
+    @property
+    def filepaths(self):
+        return netcdf.netcdf_filepaths()
